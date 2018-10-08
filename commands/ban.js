a5d5caf80fe4a1f3eb054b66e052d1526f616737
FireUpdate <40933469+FireUpdate@users.noreply.github.com>
2018-10-08 15:47:54 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(bUser === message.author) return message.channel.send("Are you retarded? Why do you want to ban yourself?")
+  if(bUser === message.author) return message.channel.send("<:no:491110753537884160> | Are you retarded? Why do you want to ban yourself?")
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-   if(!bReason) return message.channel.send("Please provide a reason!")
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("<:tickNo:432418492667396097> **| You don't have `BAN_MEMBERS` permissons.**");
+   if(!bReason) return message.channel.send("<:no:491110753537884160> | Please provide a reason!")
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("<:no:491110753537884160> **| You don't have `BAN_MEMBERS` permissons.**");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("Ban")
@@ -16,14 +16,14 @@ module.exports.run = async (bot, message, args) => {
     .addField("Reason", `${bReason ? bReason : "None."}`)
     .setTimestamp()
     let incidentchannel = message.guild.channels.find(`name`, "mod-log");
-    if(!incidentchannel) return message.channel.send("<:tickNo:432418492667396097> **| Can't find `mod-log` channel.**");
+    if(!incidentchannel) return message.channel.send("<:no:491110753537884160> **| Can't find `mod-log` channel.**");
     let embed = new Discord.RichEmbed()
     .setTitle("BAN")
     .addField("Banned In", message.guild.name)
     .setColor("#bc0000")
     .addField("Moderator", message.author.tag)
     .addField("Reason", `${bReason ? bReason : "None."}`)
-  message.channel.send("<:tickYes:432418492889694210> **| That member has been banned.**")
+  message.channel.send("<:yes:491110756985864213> **| That member has been banned.**")
   bUser.ban(bReason)
   incidentchannel.send(banEmbed);
   try{bUser.send(embed)}catch(e){message.channel.send("Unable to send message to user.")}
